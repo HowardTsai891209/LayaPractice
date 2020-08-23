f7392e9cc167b10c7c1d1420f91770f5eba37d21
HowardTsai891209 <b0922183548@gmail.com>
2020-08-23 13:25:43 +0800

    bug fixed

--- MsgPractice/src/Scripts/Diolog.ts
+++ MsgPractice/src/Scripts/Diolog.ts
@@ -6,21 +6,21 @@ export default class DiaLab extends ui.test.SceneUI {
     private lineComplete: boolean;
     private splitted: string;
     private showWord: number;
-    private story: string[] = ["一二三四五六七",
-        "一二三四五六七八",
-        "一二三四五六七八九",
-        "一二三四五六七八九十",
-        "一二三四五六七八九十1",
-        "一二三四五六七八九十12",
+    private story: string[] = ["在非洲，每六十秒，就有一分鐘過去",
+        "凡是每天喝水的人，有高機率在100年內死去",
+        "誰能想的到，這名16歲少女，在四年前，只是一名12歲少女",
+        "台灣人在睡覺時，大多數的美國人都在工作",
+        "當蝴蝶在南半球拍了兩下翅膀，牠就會稍微飛高一點點",
+        "據統計，未婚生子的人數中有高機率為女性",
     ];
     constructor() {
         super();
         DiaLab.instance = this;
     }
     isEnd(): void{
-        if(this.story[this.line] == undefined){ 
-            clearInterval(this.showWord);             
-            this.Diolog.off(Laya.Event.CLICK, this, this.ClickFn);  
+        if(this.story[this.line+1] == undefined){ 
+            this.Diolog.off(Laya.Event.CLICK, this, this.ClickFn);
+            console.log("結束");
             return;     
         }else if (this.story[this.word] == undefined) {
             this.word = 0;
@@ -38,8 +38,9 @@ export default class DiaLab extends ui.test.SceneUI {
     }
 
     ShowWords() : void {
-        this.showWord = setInterval(()=>{  
-            this.splitted = this.story[this.line][this.word];//.split("");   
+        this.showWord = setInterval( ()=>{  
+            this.splitted = this.story[this.line][this.word];//.split("");
+            if(this.splitted == undefined) return;   
             this.Diolog.text += this.splitted;//[this.word];
             this.word ++;            
             if(this.story[this.line][this.word] == undefined){
